89633a011f57fc8f2f05083e75c4612a56acea77
Thorben Jensen <jensen.thorben@googlemail.com>
2018-07-15 17:58:05 +0200

    code formatting

--- src/life_vault.py
+++ src/life_vault.py
@@ -13,16 +13,19 @@ import time
 
 #%% DIALOGUE FUNCTIONS
 
+
 def display_title_bar():
     os.system('clear')
     print('------------------------------')
     print('--------- LIFE VAULT ---------')
     print('------------------------------\n')  
 
+
 def show_commands():
     print('Please choose from the following commands.\n')
     for command in commands.keys():
         print(command + ':\t' + commands[command])
+
 
 def user_input(prompt):   
     command = input('\n' + prompt)
@@ -32,12 +35,14 @@ def user_input(prompt):
         show_commands()
         command = user_input(prompt)
     return command
-    
+
+
 def execute_command(command):
     if command not in commands.keys():
         raise Exception('Command %s is not implemented.' % command)
     methods[command]()
-    
+
+
 def dialogue():
     display_title_bar()
     show_commands()
@@ -46,18 +51,19 @@ def dialogue():
     # repeat
     dialogue()    
 
-#%% ACTION FUNCTIONS
 
+#%% ACTION FUNCTIONS
 def command_quit():
     print('\nGoodbye.')
     exit
     quit    
     sys.exit()
-    
+
+
 def command_google():
     print('To be implemented... returning to main menu.')
     time.sleep(5)
-    
+   
 
 #%% GLOBAL INFORMATION
 
