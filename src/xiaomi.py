c96196b3ec43dd9262ea59149a92cdfc7d428fe2
Thorben Jensen <jensen.thorben@googlemail.com>
2018-07-15 20:29:27 +0200

    adding plots to evaluation script

--- src/xiaomi.py
+++ src/xiaomi.py
@@ -4,6 +4,8 @@ from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 import sqlite3
 import pandas as pd
+from matplotlib import pyplot as plt
+
 
 # authenticate
 gauth = GoogleAuth()
@@ -28,7 +30,28 @@ df = df[(df.HEART_RATE < 255) & (df.HEART_RATE > 0)]
 df.describe()
 
 # plot histogram
-# TODO
+plt.figure()
+hist = df.HEART_RATE.hist(bins=40, xrot=45)
+fig = hist.get_figure()
+fig.savefig('results/histogram.png')
 
-# plot time series
-# TODO
+# heatrates per date (median and lower quartile)
+df2 = df
+df2['TIMESTAMP'] = pd.to_datetime(df2['TIMESTAMP'], unit='s')
+df2['date'] = df2.TIMESTAMP.dt.date
+df_median = df2.groupby('date')['HEART_RATE'].median()
+df_lower_quartile = df2.groupby('date')['HEART_RATE'].quantile(q=0.25)
+
+# plot median over time
+plt.figure()
+line = df_median.plot.line()
+fig = line.get_figure()
+fig.savefig('results/timeseries_median.png', )
+
+# plot lower quartile over time
+plt.figure()
+line = df_median.plot.line()
+fig = line.get_figure()
+locs, labels = plt.xticks()
+plt.setp(labels, rotation=45)
+fig.savefig('results/timeseries_lower_quartile.png', bbox_inches='tight')
