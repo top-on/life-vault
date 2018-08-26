40e0b6899fe78f36beb04446bf2771a1136d6d1f
Thorben Jensen <jensen.thorben@googlemail.com>
2018-08-26 14:53:58 +0200

    finishing visualization of heart_rate and step data

--- src/2_plot_data.R
+++ src/2_plot_data.R
@@ -1,5 +1,6 @@
 # VISUALIZE TRACKING DATA
 
+library(tidyr)
 library(dplyr)
 library(RSQLite)
 library(lubridate)
@@ -10,35 +11,55 @@ con <- dbConnect(dbDriver("SQLite"), dbname = "data/xiaomi.sqlite")
 
 activity <-
   dbGetQuery(con,
-             "SELECT timestamp, steps, heart_rate
+             "SELECT timestamp AS timestamp, steps AS steps,
+             heart_rate AS heart_rate
              FROM mi_band_activity_sample")
 
 dbDisconnect(con)
 
 # PREPROCESS ----
-act_clean <-
+# clean data fields
+activity_clean <-
   activity %>%
-  # timestamp to datetime and date 
-  mutate(datetime = as.POSIXct(TIMESTAMP, origin="1970-01-01")) %>% 
-  mutate(date = as_date(datetime)) %>% 
-  select(-TIMESTAMP) %>% 
+  # timestamp to datetime
+  mutate(datetime = as.POSIXct(timestamp, origin = "1970-01-01")) %>% 
+  select(-timestamp) %>% 
   # define NA values
-  mutate(STEPS = ifelse(STEPS == 0, NA, STEPS)) %>% 
-  mutate(HEART_RATE = ifelse(HEART_RATE == 0 | HEART_RATE == 255,
-                             NA, HEART_RATE)) %>%
+  mutate(steps = ifelse(steps <= 0, NA, steps)) %>% 
+  mutate(heart_rate = ifelse(heart_rate <= 0 | heart_rate == 255,
+                             NA, heart_rate)) %>%
   # filter out rows without data
-  filter(!is.na(STEPS) | !is.na(HEART_RATE))
+  filter(!is.na(steps) | !is.na(heart_rate))
 
-# VISUALIZE ----
-
-# daily heart rate over time
-act_clean %>% 
-  group_by(date) %>% 
-  summarise(median_hr = median(HEART_RATE, na.rm = T)) %>% 
-  ungroup %>% 
-  ggplot(data = ., aes(x = date, y = median_hr)) +
-  geom_line() +
+# DISTRIBUTIONS ----
+# histogram of individual heart rate measurements
+ggplot(data = activity_clean, aes(x = heart_rate)) + 
+  geom_histogram() +
   theme_bw()
 
-# step activity over time
-# TODO
+# histogram of steps per day
+activity_clean %>% 
+  group_by(date = floor_date(datetime, unit = "day")) %>% 
+  summarise(steps_sum = sum(steps, na.rm = T)) %>% 
+  ungroup %>% 
+  ggplot(data = ., aes(x = steps_sum)) + 
+  geom_histogram() +
+  theme_bw()
+
+
+# AGGREGATED TIME SERIES ----
+# aggregate time intervals
+activity_agg <-
+  activity_clean %>% 
+  group_by(date = floor_date(datetime, unit = "day")) %>% 
+  summarise(heart_rate_median = median(heart_rate, na.rm = T),
+            steps_sum = sum(steps, na.rm = T)) %>% 
+  ungroup
+
+# daily heart rate over time
+activity_agg %>%
+  gather(measurement, value, -date) %>% 
+  ggplot(data = ., aes(x = date, y = value)) +
+  geom_line() +
+  facet_grid(measurement ~ ., scales = "free_y") +
+  theme_bw()
